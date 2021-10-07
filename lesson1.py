12ecb9be3e5bcdc945baee5f3aa83e10c3567354
tyrrannt <shakirov.vitaliy@gmail.com>
2021-10-07 15:32:51 +0300

    Initial commit

---
+++ lesson1.py
@@ -0,0 +1,18 @@
+import requests
+import json
+from pprint import pprint
+# Задаем имя пользователя github
+username = "tyrrannt"
+token = ""
+# формируем url для запроса
+github_data_url = f"https://api.github.com/users/{username}"
+# запрашиваем данные в json
+github_user_data = requests.get(github_data_url).json()
+result_dict = dict()
+result_dict['author'] = github_user_data['name']
+result_dict['repository'] = []
+github_repository_data = requests.get(github_user_data['repos_url']).json()
+for item in github_repository_data:
+    result_dict['repository'].append(item['html_url'])
+json_result = json.dumps(result_dict)
+pprint(json_result)
