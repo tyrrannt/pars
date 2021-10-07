f35eb485cac60d0d814d1521f80fe98b9a8b0583
tyrrannt <shakirov.vitaliy@gmail.com>
2021-10-07 15:36:38 +0300

    save the dictionary in json and save the result to a file

--- lesson1.py
+++ lesson1.py
@@ -14,5 +14,5 @@ result_dict['repository'] = []
 github_repository_data = requests.get(github_user_data['repos_url']).json()
 for item in github_repository_data:
     result_dict['repository'].append(item['html_url'])
-json_result = json.dumps(result_dict)
-pprint(json_result)
+with open(f'{username}.json', 'w') as outfile:
+    json.dump(result_dict, outfile)
